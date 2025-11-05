ccdeee8511350f15d76f2a87b05aeb2ae4fe066c
soyranbo39-web <soy.ranbo39@gmail.com>
2025-11-04 21:13:46 -0600

    Update ICarta.cs

--- Juegos de Cartas/Interfaces/ICarta.cs
+++ Juegos de Cartas/Interfaces/ICarta.cs
@@ -5,6 +5,7 @@ namespace Juegos_de_Cartas.Interfaces;
 public interface ICarta<TValueCarta>
 {
     TValueCarta Valor { get; }
+    
     String ObtenerDescripcionDeCarta();
 
     bool EsCartaJugable(ICarta<TValueCarta> carta);
