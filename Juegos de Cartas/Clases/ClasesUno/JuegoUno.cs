9749cec873ab3a7db15ad64fb195c69e039422f0
Abraham089 <abrahamgb274@gmail.com>
2025-11-06 10:04:57 -0600

    Juegos principales
    Cree la interfaz IJuegos para los dos juegos y cree una carpeta para las clases de las cartas de uno

---
+++ Juegos de Cartas/Clases/ClasesUno/JuegoUno.cs
@@ -0,0 +1,32 @@
+using System;
+using Juegos_de_Cartas.Interfaces;
+
+namespace Juegos_de_Cartas.Clases.ClasesUno;
+
+public class JuegoUno : IJuegos<CartaUnoAbstracta>
+{
+  public void IniciarJuego()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void MovimientoDeCartas()
+  {
+    throw new NotImplementedException();
+  }
+
+  public bool SePuedeJugar()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void SiguienteJugador()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void TomarDecision()
+  {
+    throw new NotImplementedException();
+  }
+}
