3ddcc8baf03cd69f23b96ae863e4de2424bf1425
soyranbo39-web <soy.ranbo39@gmail.com>
2025-11-08 20:53:48 -0600

    Se refactorizo una clase y se elimino una clase
    Se elimino una clase que no se ocuparia debido a que se le dio un nombre apropiado y se elimino para evitar conlfictos

--- Juegos de Cartas/Clases/ClasesBlackJack/Deckjack.cs
+++ Juegos de Cartas/Clases/ClasesBlackJack/Deckjack.cs
@@ -4,18 +4,17 @@ using Juegos_de_Cartas.Interfaces;
 namespace Juegos_de_Cartas.Clases.ClasesBlackJack;
 
 public class DeckJack
-{
-  
-             public static Deck<ICartaPoker> ConstruirMazoCompleto()
-        {
-            var generadorCartas = new GeneradorCartasJack();
-            var cartas = generadorCartas.GenerarCartasCompletas();
+{            
+ public static Deck<ICartaPoker> ConstruirMazoCompleto()
+    {
+        var generadorCartas = new GeneradorCartasJack();
+        var cartas = generadorCartas.GenerarCartasCompletas();
 
-            var deck = new Deck<ICartaPoker>(cartas);
-            var barajador = new BarajeadorDeck();
-            barajador.Barajear(deck);
+        var deck = new Deck<ICartaPoker>(cartas);
+        var barajador = new BarajeadorDeck();
+        barajador.Barajear(deck);
 
-            return deck;
-        }
+        return deck;
+    }
     
 }
