2ebc7eecfc38ec63da29032ef4d3852899532ef3
soyranbo39-web <soy.ranbo39@gmail.com>
2025-11-10 16:24:13 -0600

    se cambio cierta logica

--- Juegos de Cartas/Interfaces/IPuntosJack.cs
+++ Juegos de Cartas/Interfaces/IPuntosJack.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Juegos_de_Cartas.Interfaces;
 
-public interface IPuntosJack
+public interface IPuntosCalculadora<ICarta> where ICarta : class
 {
-
+  int CalcularPuntos(IEnumerable<ICarta> cartas);
 }
